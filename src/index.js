2f814b6ac6e13179e7b2b7509e86a48617ae1daa
claus-lut <71755294+claus-lut@users.noreply.github.com>
2020-11-06 00:36:09 +0200

    Added javascript functions

--- src/index.js
+++ src/index.js
@@ -9,63 +9,6 @@ app.set("view engine", "pug");
 //  res.send("hellotttte test");
 //});
 
-var tabletext = "";
-var i, j;
-var cursor = "x";
-var player = 1;
-
-function cellClick() {
-  if (this.innerHTML !== "o" && this.innerHTML !== "x") {
-    this.innerHTML = cursor; // The function returns the product of p1 and p2
-    checkWinner();
-    if (cursor == "o") {
-      cursor = "x";
-      player = 1;
-    } else {
-      cursor = "o";
-      player = 2;
-    }
-  }
-}
-
-function checkWinner() {
-  var table = document.getElementById("ttttable");
-  // Check columns
-  for (var i = 0; i < 5; i++) {
-    var winner = true;
-    for (var j = 0; j < 5; j++) {
-      if (table.rows[i].cells[j].innerHTML !== cursor) {
-        winner = false;
-      }
-    }
-    if (winner) {
-      alert("Player " + player + " won!");
-    }
-  }
-  // Check rows
-  for (var i = 0; i < 5; i++) {
-    var winner = true;
-    for (var j = 0; j < 5; j++) {
-      if (table.rows[j].cells[i].innerHTML !== cursor) {
-        winner = false;
-      }
-    }
-    if (winner) {
-      alert("Player " + player + " won!");
-    }
-  }
-  // Check diagonal
-  for (var i = 0; i < 5; i++) {
-    var winner = true;
-    if (table.rows[i].cells[i].innerHTML !== cursor) {
-      winner = false;
-    }
-  }
-  if (winner) {
-    alert("Player " + player + " won!");
-  }
-}
-
 app.get("/", function (req, res) {
   res.render("index", { title: "Hey", message: "Hello there!" });
 });
